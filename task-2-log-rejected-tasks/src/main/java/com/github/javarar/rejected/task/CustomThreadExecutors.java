35a0199fba2e3e5829e04c88f4d36544007e9735
Татьяна Николаева <72814164+Tatiana-N@users.noreply.github.com>
2024-08-01 08:47:08 +0300

    CustomThreadExecutors

--- task-2-log-rejected-tasks/src/main/java/com/github/javarar/rejected/task/CustomThreadExecutors.java
+++ task-2-log-rejected-tasks/src/main/java/com/github/javarar/rejected/task/CustomThreadExecutors.java
@@ -2,13 +2,49 @@ package com.github.javarar.rejected.task;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Logger;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public final class CustomThreadExecutors {
-
-    public static Executor logRejectedThreadPoolExecutor(/*можно передать такие аргументы, которые помогут вам сконструировать нужный пул*/) {
-        throw new UnsupportedOperationException("реализуй меня");
+public final class
+CustomThreadExecutors implements  Executor{
+    private final Logger logger = Logger.getLogger(CustomThreadExecutors.class.getSimpleName());
+    private Queue<Runnable> tasks;
+    private List<Thread> threads = new LinkedList<>();
+    
+    private CustomThreadExecutors(int sizeQueueTasks, int threadNumber) {
+        tasks = new LinkedBlockingQueue<>(sizeQueueTasks);
+        for (int i = 0; i < threadNumber; i++) {
+            threads.add(new Thread(() -> {
+                while (!Thread.currentThread().isInterrupted()) {
+                    Runnable runnable = tasks.poll();
+                    if(runnable != null) {
+                        runnable.run();
+                    }
+                }
+            }));
+        }
+        threads.forEach(Thread::start);
     }
-}
+    
+    public static Executor logRejectedThreadPoolExecutor(Integer sizeQueueTasks, Integer threadNumber ) {
+        return new CustomThreadExecutors(sizeQueueTasks, threadNumber);
+    }
+    
+    @Override
+    public void execute(Runnable command) {
+            try{
+                tasks.add(command);
+            } catch (IllegalStateException e){
+                logger.warning("я не ругаюсь, что много задач, но это уже перебор");
+            }
+        }
+    }
--- task-2-log-rejected-tasks/src/test/java/com/github/javarar/rejected/task/CustomTreadExecutorsTest.java
+++ task-2-log-rejected-tasks/src/test/java/com/github/javarar/rejected/task/CustomTreadExecutorsTest.java
@@ -1,11 +1,56 @@
 package com.github.javarar.rejected.task;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class CustomTreadExecutorsTest {
+import java.io.ByteArrayOutputStream;
+import java.util.concurrent.Executor;
+import java.util.logging.*;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CustomTreadExecutorsTest {
+    private final Logger logger = Logger.getLogger(CustomThreadExecutors.class.getSimpleName());
+    private ByteArrayOutputStream logContent;
+    
+    @BeforeEach
+    public void setUp() {
+        logContent = new ByteArrayOutputStream();
+        StreamHandler customHandler = new StreamHandler(logContent, new SimpleFormatter());
+        
+        LogManager.getLogManager().reset();
+        logger.addHandler(customHandler);
+        logger.setUseParentHandlers(false); // Отключаем обработчики по умолчанию
+    }
+    
+    
+    
     @Test
     public void threadPoolDoesNotThrowExceptionOnQueueOverflow() {
-        throw new UnsupportedOperationException("реализуй меня");
+        // 0 тредов чтобы не забрали таски которыми наполняем
+        Executor executor = CustomThreadExecutors.logRejectedThreadPoolExecutor(3, 0);
+        for (int i = 0; i < 3; i++) {
+            executor.execute(() -> {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+        System.out.println("наполнили очередь задач");
+        
+        executor.execute(() -> {
+            System.out.println("лишняя задача");
+        });
+        // Принудительное завершение обработчика, чтобы все логи были записаны
+        for (Handler handler : logger.getHandlers()) {
+            handler.flush();
+        }
+    
+        // Проверка логов на наличие сообщения об отклонении задачи
+        String logOutput = logContent.toString();
+        assertTrue(logOutput.contains("я не ругаюсь, что много задач, но это уже перебор"), "Expected log message about task rejection");
+    
     }
 }
