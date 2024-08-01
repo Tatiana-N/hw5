2f935995bc02c58184dee83bd17bbf5fb0530978
Татьяна Николаева <72814164+Tatiana-N@users.noreply.github.com>
2024-08-01 19:22:25 +0300

    LuckyTicket

--- task-3-lucky-ticket/src/main/java/com/github/javarar/lucky/ticket/LuckyTicket.java
+++ task-3-lucky-ticket/src/main/java/com/github/javarar/lucky/ticket/LuckyTicket.java
@@ -8,19 +8,19 @@ import java.util.stream.Collectors;
 
 public class LuckyTicket extends RecursiveTask<List<List<Integer>>> {
     final int seqThreshold;
-    private final int start;
-    private final int end;
+    private final long start;
+    private final long end;
     private final Integer size;
     
-    public LuckyTicket(Integer start, Integer end, Integer size) {
-        this.seqThreshold = (10 * size) / 2;
+    public LuckyTicket(Integer seqThreshold, Long start, Long end, Integer size) {
+        this.seqThreshold = seqThreshold;
         this.start = start;
         this.end = end;
         this.size = size;
     }
     
     public static void main(String[] args) {
-        System.out.println(luckyTicketProbability(10));
+        System.out.println(luckyTicketProbability(8));
     }
     
     public static int luckyTicketProbability(int serialNumberLength) {
@@ -28,8 +28,8 @@ public class LuckyTicket extends RecursiveTask<List<List<Integer>>> {
             throw new IllegalArgumentException("Странные у вас билеты, считайте - все счастливые");
         }
         long start = System.currentTimeMillis();
-        int numberOfTickets = (int) Math.pow(10, serialNumberLength);
-        LuckyTicket luckyTicket = new LuckyTicket(0, numberOfTickets, serialNumberLength);
+        long numberOfTickets = (long) Math.pow(10, serialNumberLength);
+        LuckyTicket luckyTicket = new LuckyTicket((int)Math.sqrt(numberOfTickets), 0L, numberOfTickets, serialNumberLength);
         luckyTicket.fork();
         List<List<Integer>> join = luckyTicket.join();
         System.out.println(System.currentTimeMillis() - start);
@@ -41,7 +41,7 @@ public class LuckyTicket extends RecursiveTask<List<List<Integer>>> {
     protected List<List<Integer>> compute() {
         if ((end - start) < seqThreshold) {
             List<List<Integer>> list = new ArrayList<>();
-            for (int i = start; i < end; i++) {
+            for (long i = start; i < end; i++) {
                 int firstHalf = sumDigits(i / seqThreshold);
                 int secondHalf = sumDigits(i % seqThreshold);
                 if (firstHalf == secondHalf) {
@@ -50,8 +50,8 @@ public class LuckyTicket extends RecursiveTask<List<List<Integer>>> {
             }
             return list;
         } else {
-            LuckyTicket subLuckyTicket1 = new LuckyTicket( start, start + (end - start) / 2, size);
-            LuckyTicket subLuckyTicket2 = new LuckyTicket(start + (end - start) / 2, end, size);
+            LuckyTicket subLuckyTicket1 = new LuckyTicket( seqThreshold, start, start + (end - start) / 2, size);
+            LuckyTicket subLuckyTicket2 = new LuckyTicket(seqThreshold, start + (end - start) / 2, end, size);
             List<List<Integer>> result = new ArrayList<>();
             subLuckyTicket1.fork();
             subLuckyTicket2.fork();
@@ -61,12 +61,12 @@ public class LuckyTicket extends RecursiveTask<List<List<Integer>>> {
         }
     }
     
-    private List<Integer> getList(int i, int serialNumberLength) {
+    private List<Integer> getList(long i, int serialNumberLength) {
         String s = String.format("%0" + serialNumberLength + "d", i);
         return Arrays.stream(s.split("")).map(Integer::valueOf).collect(Collectors.toList());
     }
     
-    private int sumDigits(int i) {
+    private int sumDigits(long i) {
         int sum = 0;
         while (i > 0) {
             sum += i % 10;
